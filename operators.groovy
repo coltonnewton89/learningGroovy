5a1f44012c9b2bab7603a766cfea1c61aaa2ff57
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2021-01-18 20:16:20 -0600

    first commit to learningGroovy

---
+++ operators.groovy
@@ -0,0 +1,12 @@
+package com.test.demo
+
+
+// normal arithmetic operators
+assert 1 + 2 == 3
+assert 4 - 3 == 1
+assert 3 * 5 == 15
+assert 3 / 2 == 1.5
+assert 10 % 3 == 1
+/*throw intentional err*/
+assert 2 ** 3 == 7
+
---
+++ variables.groovy
@@ -0,0 +1,17 @@
+package com.test.demo
+
+def (a,b,c) = [12,15,18]
+println a
+println c
+
+byte myByte = 9
+println myByte
+
+int myInt = 10
+println myInt
+
+boolean myBoo = true
+println myBoo
+
+String myName = "Colton"
+println "Hello, my name is ${myName}"
